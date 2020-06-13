47e0957ac9e1e2e56adcbecc06c9598db4c651a9
Annie Chakraborty <the.demented.genius29@gmail.com>
2020-06-13 15:36:11 +0530

    Completed the basic customisation of the app and imported shared preferences package to dynamically store and display events.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
 import 'package:table_calendar/table_calendar.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() => runApp(MyApp());
 
@@ -71,7 +72,44 @@ class _HomePageState extends State<HomePage> {
                 formatButtonPadding: EdgeInsets.all(12.0),
                 formatButtonShowsNext: false,
               ),
-              startingDayOfWeek: StartingDayOfWeek.saturday,
+              startingDayOfWeek: StartingDayOfWeek.monday,
+//              Deciding actions to occur when a date os selected
+              onDaySelected: (date, events){
+                print(date.toIso8601String());
+              },
+//              We can provide more customisations using builders
+              builders: CalendarBuilders(
+                selectedDayBuilder: (context, date, event) => Container(
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+//                    color: Theme.of(context).primaryColor,
+                    color: Colors.deepPurpleAccent,
+                    shape: BoxShape.circle,
+                  ),
+                  child: Text(
+                    date.day.toString(),
+                    style: TextStyle(
+                      color : Colors.white,
+                      fontStyle: FontStyle.italic,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                todayDayBuilder: (context, date, event) => Container(
+                  alignment: Alignment.center,
+                  child: Text(
+                    date.day.toString(),
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  decoration: BoxDecoration(
+                    color: Colors.teal.shade800,
+                    shape: BoxShape.circle,
+                  ),
+                )
+              ),
             )
           ],
         ),
